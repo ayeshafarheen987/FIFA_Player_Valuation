d973f523323d866464388961afea92bd90799a8e
ayeshafarheen987 <ayeshafarheen.mindsnxt@gmail.com>
2022-10-11 16:10:37 +0530

    V1

---
+++ web_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import pickle
+#import streamlit.components.v1 as components
+
+st.title('Fifa Player Market Value')
+
+Age = st.number_input("Enter Age",0,100)
+Best_overall = st.number_input("Enter Best Overall",0,100)
+Overall_rating = st.number_input("Enter Overall Rating",0,100)
+Potential = st.number_input("Enter Potential",0,100)
+
+if st.button("Predict"):
+
+    pickle_in = open('model_Random.pkl', 'rb') 
+    r = pickle.load(pickle_in)
+    
+    X = pd.DataFrame([[Age,Best_overall,Overall_rating,Potential]], columns = ['Age','Best_overall','Overall_rating','Potential'])
+    prediction = r.predict(X)
+    
+    st.success(f'The Market Value of Player is {prediction} Euro')
+    
