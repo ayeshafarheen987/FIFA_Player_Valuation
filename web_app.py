0c39f19d1c9bbe73646ab692e73d6737fd37d4ad
ayeshafarheen987 <113899070+ayeshafarheen987@users.noreply.github.com>
2022-11-12 18:36:25 +0530

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -4,7 +4,6 @@ import pickle
 #import streamlit.components.v1 as components
 
 st.title('Fifa Player Market Value')
-
 Age = st.number_input("Enter Age",0,100)
 Best_overall = st.number_input("Enter Best Overall",0,100)
 Overall_rating = st.number_input("Enter Overall Rating",0,100)
@@ -15,8 +14,10 @@ if st.button("Predict"):
     pickle_in = open('model_Random.pkl', 'rb') 
     r = pickle.load(pickle_in)
     
-    X = pd.DataFrame([[Age,Best_overall,Overall_rating,Potential]], columns = ['Age','Best_overall','Overall_rating','Potential'])
+    X = pd.DataFrame([[Best_overall,Overall_rating,Potential,Age]], columns = ['Best_overall','Overall_rating','Potential','Age'])
     prediction = r.predict(X)
     
     st.success(f'The Market Value of Player is {prediction} Euro')
+    
+    
     
